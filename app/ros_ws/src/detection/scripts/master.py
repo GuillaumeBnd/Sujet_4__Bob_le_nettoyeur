90837c74db78c8ddadb5d65eaab36fff2e950286
Jules Graeff <jules.graeff@tpb00.cpe.fr>
2020-10-15 12:21:14 +0200

    progress on master node

--- app/ros_ws/src/detection/scripts/master.py
+++ app/ros_ws/src/detection/scripts/master.py
@@ -22,10 +22,29 @@ class Master:
         rospy.loginfo('"master" node has been created')
 
         rospy.Subscriber('/vide_detection', Vide, self._vide_detection_callback)
-     
+        rospy.Subscriber('/command_mode', String, self._command_mode_callback)
+        rospy.Subscriber('/command_roues', String, self._command_roues_callback)
+        rospy.Subscriber('/command_spray', Bool, self._command_command_spray_callback)
+        rospy.Subscriber('/command_eponge', Bool, self._command_command_eponge_callback)
+
+        if self._mode == 'auto':
+            pass
+
+        elif self._mode == 'control':
+            pass
+
+        else:
+            raise Exception('Issue with the mode value.')
+
+
     def _vide_detection_callback (self, data):
 
         rospy.loginfo(f"Vide Detection = {bool(data.detected)}")
+
+    def _command_mode_callback (self, data):
+
+        self._mode = data.data
+        rospy.loginfo(f"Command sent via BLE = {bool(data.data)}")
 
 ######################################################################################################################################################
 
