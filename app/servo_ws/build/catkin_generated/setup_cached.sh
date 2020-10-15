9f78723a8b1a64f0f5d80ce7d83f9b7976b5f967
Antoine Passemard <antoine.passemard@tpb01.cpe.fr>
2020-10-15 12:34:09 +0200

    code Antoine

---
+++ app/servo_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/fs03/share/users/antoine.passemard/home/servo_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/fs03/share/users/antoine.passemard/home/servo_ws/devel/lib:$LD_LIBRARY_PATH"
+export PWD='/fs03/share/users/antoine.passemard/home/servo_ws/build'
+export PYTHONPATH="/fs03/share/users/antoine.passemard/home/servo_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/fs03/share/users/antoine.passemard/home/servo_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/fs03/share/users/antoine.passemard/home/servo_ws/src:$ROS_PACKAGE_PATH"
---
+++ app/servo_ws/src/my_dynamixel_tutorial/script/gestion_speed_roues.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import Float64
+from dynamixel_msgs.msg import MotorStateList
+
+class gestion_roue:
+
+    def __init__(self):
+
+        rospy.init_node('gestion_speed_roues', anonymous=True)
+        self.speed_roue_gauche = rospy.Publisher('/joint1_controller/command', Float64, queue_size=10)
+        self.speed_roue_droite = rospy.Publisher('/joint2_controller/command', Float64, queue_size=10)
+        self.position_eponge = rospy.Publisher('/joint3_controller/command', Float64, queue_size=10)
+        self.speed_motor = rospy.Publisher('/motor_states/pan_tilt_port', MotorStateList, queue_size=10)
+        rospy.sleep(10)
+        rospy.loginfo("changemement de vitesse")
+        self.speed_roue_gauche.publish(3)
+        self.speed_roue_droite.publish(7)
+        rospy.sleep(10)
+        self.speed_roue_gauche.publish(0)
+        self.speed_roue_droite.publish(0)
+        self.position_eponge.publish(1)
+
+if __name__ == '__main__':
+    c = gestion_roue()
+    rospy.spin()
+
